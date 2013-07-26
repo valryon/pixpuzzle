bdc4c33ce77b249568afaa7c00beda2f00cf274c
Damien Mayance <valryon@gmail.com>
2013-07-26 23:03:05 +0200

    Storyboard skeleton

---
+++ PixPuzzle.iOS/Controllers/Menu/MenuFriendsViewController.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Drawing;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace PixPuzzle
+{
+	public partial class MenuFriendsViewController : UIViewController
+	{
+		public MenuFriendsViewController () : base ()
+		{
+		}
+
+	}
+}
+
