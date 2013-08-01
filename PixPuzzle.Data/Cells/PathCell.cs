e5bae9918acd0407fd15b6a23962735a3f475f2e
Damien Mayance <valryon@gmail.com>
2013-08-01 08:07:53 +0200

    Simplifying: one cell type and one interface

--- PixPuzzle.Data/Cells/PathCell.cs
+++ PixPuzzle.Data/Cells/PathCell.cs
@@ -5,20 +5,27 @@ namespace PixPuzzle.Data
     /// <summary>
     /// One cell of the path puzzle
     /// </summary>
-    public class PathCell : Cell
+    public class PathCell
     {
+		#region Constructor
+
 		public PathCell ()
-			: base()
 		{
 		}
 
         public PathCell(int x, int y)
-			: base(x,y)
         {
+			X = x;
+			Y = y;
+
             // Default values
             Path = null;
             IsPathStartOrEnd = false;
         }
+
+		#endregion
+
+		#region Methods
 
         /// <summary>
         /// Sets the number to display. It also means that the cell is a path start or end.
@@ -31,8 +38,48 @@ namespace PixPuzzle.Data
             // The cell is the beginning or the end of a path
             Path = new Path(this, pathLength);
         }
+		
+		public override string ToString ()
+		{
+			return string.Format ("X:{0} Y:{1} Color:{2}", X, Y, Color);
+		}
+
+		#endregion
 
         #region Properties
+
+		/// <summary>
+		/// Location (X)
+		/// </summary>
+		/// <value>The x.</value>
+		public int X {
+			get;
+			set;
+		}
+		/// <summary>
+		/// Location (Y)
+		/// </summary>
+		/// <value>The y.</value>
+		public int Y {
+			get;
+			set;
+		}
+		/// <summary>
+		/// Set the right color form the image
+		/// </summary>
+		/// <value>The color.</value>
+		public CellColor Color {
+			get;
+			set;
+		}
+		/// <summary>
+		/// Cells has been marked by grid creator
+		/// </summary>
+		/// <value><c>true</c> if this instance is marked; otherwise, <c>false</c>.</value>
+		public bool IsMarked {
+			get;
+			set;
+		}
 
         /// <summary>
         /// Tells if we are on a cell that is the start or the end of a complete path
--- PixPuzzle.Data/Views/IPathGridView.cs
+++ PixPuzzle.Data/Views/IPathGridView.cs
@@ -7,8 +7,22 @@ using Microsoft.Xna.Framework;
 
 namespace PixPuzzle.Data
 {
-	public interface IPathGridView : IGridView<PathCell>
+	public interface IPathGridView
 	{
+		void InitializeViewForDrawing ();
+
+		void OrderRefresh (Rectangle zoneToRefresh);
+
+		void StartDraw ();
+
+		bool IsToRefresh (PathCell cell, Rectangle cellRect);
+
+		void DrawGrid ();
+
+		void DrawCellBase (PathCell cell,Rectangle rectangle);
+
+		void EndDraw ();
+
 		void DrawPath (PathCell cell,Rectangle pathRect, Point direction, CellColor color);
 
 		void DrawLastCellIncompletePath (PathCell cell,Rectangle rect, string pathValue, CellColor color);
